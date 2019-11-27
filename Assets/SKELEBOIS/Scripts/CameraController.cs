d9b28e60e5598091840cf228d7f0eb4f704bcd42
GamesDevDave <GamesDevDave@gmail.com>
2019-11-27 21:10:04 +0000

    Added CameraController.cs to establish Camera at head height of player. Mouse movement now affects where player looks. Movement script is affected by rotation - needs fixing.

---
+++ Assets/SKELEBOIS/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    public float headHeight;
+    public float minimumXAngle = -60f;
+    public float maximumXAngle = 60f;
+    public float MinimumYAngle = -360f;
+    public float maximumYAngle = 360f;
+    public float sensitivityX;
+    public float sensitivityY;
+
+    private float rotationX;
+    private float rotationY;
+
+    void Awake()
+    {
+        mainCamera.transform.position = new Vector3(gameObject.transform.position.x, headHeight, gameObject.transform.position.z);
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        GetMouseMovement();
+        PlayerCameraRotation();
+    }
+
+    private void GetMouseMovement()
+    {
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+        rotationX = Mathf.Clamp(rotationX, minimumXAngle, maximumXAngle);
+    }
+
+    private void PlayerCameraRotation()
+    {
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        mainCamera.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+    }
+}
---
+++ Assets/SKELEBOIS/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float walkSpeed;
+
+    private Rigidbody rb;
+    private float horizontalMovement;
+    private float verticalMovement;
+    private Vector3 movementDirection;
+    private Vector3 yVelocityFix;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetMovementDirection();
+        Move();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void Move()
+    {
+        yVelocityFix = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = movementDirection * walkSpeed * Time.deltaTime;
+        rb.velocity += yVelocityFix;
+    }
+
+    private void GetMovementDirection()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+        movementDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+    }
+}
