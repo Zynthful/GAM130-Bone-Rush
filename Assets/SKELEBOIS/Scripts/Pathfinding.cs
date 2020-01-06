15ca870d8021265a03b334b9dd2c764a8b68c1dc
lr228874 <lr228874@falmouth.ac.uk>
2020-01-06 19:09:24 +0000

    started programming simple pathfinding

---
+++ Assets/SKELEBOIS/Scripts/Pathfinding.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject player;
+
+    void Start()
+    {
+        Vector3 enemy_location = transform.position;
+        //Vector3 player_location = ???
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.position.x);
+        //agent.SetDestination(ENTER PLAYERS LOCATION HERE);
+        UnityEngine.Debug.Log(player.Transform.position);
+    }
+}
