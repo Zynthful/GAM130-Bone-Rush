014f3b71d07a2bebb7df677fe58097ecbcd30075
lr228874 <lr228874@falmouth.ac.uk>
2020-01-07 15:25:52 +0000

    started coding a wandering script

--- Assets/SKELEBOIS/Scripts/Pathfinding.cs
+++ Assets/SKELEBOIS/Scripts/Pathfinding.cs
@@ -7,6 +7,16 @@ public class Pathfinding : MonoBehaviour
 {
     public NavMeshAgent agent;
     public GameObject player;
+    public float follow_distance = 10f;
+
+    void wonder() {
+        Vector3 destination = new Vector3(Random.Range(-10f, 10f), 28.16976f, Random.Range(-10f, 10f));
+        //check if position can be walked to
+        //if () { }
+        //if ture walk
+        //else find new destination
+
+    }
 
     /*
     void Start()
@@ -18,8 +28,13 @@ public class Pathfinding : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Vector3 player_location = player.transform.position;
-        agent.SetDestination(player_location);
-    }
+    {   //player is followed until the enemy dies even if the player gets out of the enemies range
+        float distance_to_player = Vector3.Distance(transform.position, player.transform.position);
+        if (distance_to_player <= follow_distance)
+        {    //player can be seen through walls
+            Vector3 player_location = player.transform.position;
+            agent.SetDestination(player_location);
+        }
+        //else {wonder();}
+    }  
 }
