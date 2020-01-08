e4cd458c75512955413b5f3db7fbf683e6042eed
lr228874 <lr228874@falmouth.ac.uk>
2020-01-08 14:31:46 +0000

    finished patrol script and added random patrol script

--- Assets/SKELEBOIS/Scripts/PatrolRandom.cs
+++ Assets/SKELEBOIS/Scripts/PatrolRandom.cs
@@ -3,36 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Patrol : MonoBehaviour
+public class PatrolRandom : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public int wait_time = 3;
     Vector3[] destinations = new[] {new Vector3(20f, 0, 20f),
         new Vector3(20f, 1.5f, -20f),
         new Vector3(-20f, 1.5f, -20f),
         new Vector3(-20f, 1.5f, 20f),};
 
-    public int set_path = 0; 
+    private int random_spot;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        agent.SetDestination(destinations[set_path]);
+        random_spot = Random.Range(0, destinations.Length);
+        agent.SetDestination(destinations[random_spot]);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 current_location = transform.position;
-        if (current_location.x == destinations[set_path].x && current_location.z == destinations[set_path].z)
+        if (current_location.x == destinations[random_spot].x && current_location.z == destinations[random_spot].z)
         {
-            if (set_path == 3)
-            {
-                set_path = -1;
-            }
-            set_path += 1;
-            agent.SetDestination(destinations[set_path]);
+            random_spot = Random.Range(0, destinations.Length);
+            agent.SetDestination(destinations[random_spot]);
         }
+
     }
 }
