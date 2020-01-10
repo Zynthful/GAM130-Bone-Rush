945de3db5d66ca8d4c9c3acf69762917616acec9
lr228874 <lr228874@falmouth.ac.uk>
2020-01-10 16:52:26 +0000

    made line of sight have a limited range and not work through walls

--- Bone Rush/Assets/Bone Rush/Scripts/LineOfSight.cs
+++ Bone Rush/Assets/Bone Rush/Scripts/LineOfSight.cs
@@ -6,11 +6,12 @@ using UnityEngine.AI;
 public class LineOfSight : MonoBehaviour
 {
     //will only chase the player if they enter their line of sight 
-    //struggles to chase players when they turn
+    //the enemies line of sight can be changed by the look_range variable
     public NavMeshAgent agent;
     public GameObject player;
     public float rotation_speed = 35;
-    public bool found_player;
+    bool found_player;
+    public float look_range = 25f;
 
     // Update is called once per frame
     void Update()
@@ -25,9 +26,9 @@ public class LineOfSight : MonoBehaviour
         {
             transform.Rotate(Vector3.up * rotation_speed * Time.deltaTime, Space.World);
 
-            int layerMask = 1 << 8;     //makes only layer 8 (player layer) will be effected by the ray
+            int layerMask = 8 << 10;     //makes only layers 8-10 be effected by the ray (player, enemies, walls)
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, look_range, layerMask))
             {
                 //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
                 //Debug.Log("Player Found");
--- Bone Rush/Assets/Bone Rush/Scripts/Pathfinding.cs
+++ Bone Rush/Assets/Bone Rush/Scripts/Pathfinding.cs
@@ -5,20 +5,28 @@ using UnityEngine.AI;
 
 public class Pathfinding : MonoBehaviour
 {
-    //will only chase the player if they enter their radius
+    //will only chase the player if they enter their view radius
+    //player can be seen through walls and is followed until the AI dies
     public NavMeshAgent agent;
     public GameObject player;
     public float follow_distance = 10f;
+    bool in_attack_radius = false;
 
 
     // Update is called once per frame
     void Update()
-    {   //player is followed until the enemy dies even if the player gets out of the enemies range
+    {  
         float distance_to_player = Vector3.Distance(transform.position, player.transform.position);
         if (distance_to_player <= follow_distance)
-        {    //player can be seen through walls
+        {    
             Vector3 player_location = player.transform.position;
             agent.SetDestination(player_location);
+
+            if (in_attack_radius == true)
+            {
+                //Face the player
+                //Attack the player
+            }
         }
     }  
 }
