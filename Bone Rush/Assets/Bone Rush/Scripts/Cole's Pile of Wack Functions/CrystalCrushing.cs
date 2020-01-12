dba4a31538e1193006b08005bf0ab43cb05363ee
ColeGGilbert <cg233192@falmouth.ac.uk>
2020-01-12 12:01:22 +0000

    Implemented basic features of crystal crushing  - After a short delay, when crushed, the player is teleported to the boss room  - Player character is not deleted during scene change  - Crush key is on input axis list, meaning the control key can be changed easily  - Default key set to "g" Also imported TextMeshPro + TMPExtras

---
+++ Bone Rush/Assets/Bone Rush/Scripts/Cole's Pile of Wack Functions/CrystalCrushing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrystalCrushing : MonoBehaviour
+{
+
+	// Comment/Uncomment when needed (i.e. if another script needs to know when the crystal is crushed)
+	//public static bool crystalCrushed { get; set; }
+	bool crystalCrushed;
+
+	float crushToTeleportDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		gameObject.transform.parent = null;
+		DontDestroyOnLoad(gameObject);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetAxis("CrushCrystal") == 1 && !crystalCrushed)
+		{
+			crystalCrushed = true;
+			crushToTeleportDelay = 2f;
+		}
+		else if(crushToTeleportDelay > 0)
+		{
+			crushToTeleportDelay -= Time.deltaTime;
+		}
+		else if(crushToTeleportDelay <= 0 && crystalCrushed && SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(0))
+		{
+			CrushCrystal();
+		}
+    }
+
+	// Anything that happens when the crystal is crushed goes here
+	void CrushCrystal()
+	{
+		SceneManager.LoadScene("BossRoom");
+	}
+}
