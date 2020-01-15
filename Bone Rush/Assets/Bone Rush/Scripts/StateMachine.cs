7576214d2bff2ba81a7229af5c13d9b6203e05d5
lr228874 <lr228874@falmouth.ac.uk>
2020-01-15 18:55:55 +0000

    started working on a state machine script

--- Bone Rush/Assets/Bone Rush/Scripts/StateMachine.cs
+++ Bone Rush/Assets/Bone Rush/Scripts/StateMachine.cs
@@ -1,183 +1,91 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class StateMachine : MonoBehaviour
 {
-    public Team Team => _team;
-    [SerializeField] private Team _team;
-    [SerializeField] private LayerMask _layerMask;
+    //[SerializeField] private LayerMask _layerMask;
 
-    private float _attackRange = 3f;
-    private float _rayDistance = 5.0f;
-    private float _stoppingDistance = 1.5f;
+    Vector3[] destinations = new[] {new Vector3(20f, 0, 20f),
+        new Vector3(20f, 1.5f, -20f),
+        new Vector3(-20f, 1.5f, -20f),
+        new Vector3(-20f, 1.5f, 20f)};
+    private State _currentState;
+    public NavMeshAgent agent;
+    public GameObject player;
+    public float follow_distance = 10f;
+    public int set_path = 0;
+    public int wait_time = 3;
 
-    private Vector3 _destination;
-    private Quaternion _desiredRotation;
-    private Vector3 _direction;
-    //private Drone _target;
-    private DroneState _currentState;
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSecondsRealtime(wait_time);
+        agent.SetDestination(destinations[set_path]);
+    }
 
 
     private void Update()
     {
         switch (_currentState)
         {
-            case DroneState.Patrol:
+            case State.Patrol:
                 {
-                    if (NeedsDestination())
+                    agent.SetDestination(destinations[set_path]);
+                    float distance_to_player = Vector3.Distance(transform.position, player.transform.position);
+                    Vector3 current_location = transform.position;
+                    if (current_location.x == destinations[set_path].x && current_location.z == destinations[set_path].z)
                     {
-                        GetDestination();
+                        if (set_path == 3)
+                        {
+                            set_path = -1;
+                        }
+                        set_path += 1;
+                        StartCoroutine(WaitTime());
                     }
 
-                    transform.rotation = _desiredRotation;
 
-                    transform.Translate(Vector3.forward * Time.deltaTime * 5f);
-
-                    var rayColor = IsPathBlocked() ? Color.red : Color.green;
-                    Debug.DrawRay(transform.position, _direction * _rayDistance, rayColor);
-
-                    while (IsPathBlocked())
+                    if (distance_to_player <= follow_distance)
                     {
-                        Debug.Log("Path Blocked");
-                        GetDestination();
-                    }
-
-                    var targetToAggro = CheckForAggro();
-                    if (targetToAggro != null)
-                    {
-                        //_target = targetToAggro.GetComponent<Drone>();
-                        _currentState = DroneState.Follow;
+                        _currentState = State.Follow;
                     }
 
                     break;
                 }
-            case DroneState.Follow:
+
+            case State.Patrol_Random:
                 {
-                    /*
-                    if (_target == null)
-                    {
-                        _currentState = DroneState.Patrol;
-                        return;
-                    }
-                    */
-
-                    //transform.LookAt(_target.transform);
-                    transform.Translate(Vector3.forward * Time.deltaTime * 5f);
-
-                    /*
-                    if (Vector3.Distance(transform.position, _target.transform.position) < _attackRange)
-                    {
-                        _currentState = DroneState.Attack;
-                    }
-                    */
                     break;
                 }
-            case DroneState.Attack:
+
+            case State.Follow:
                 {
-                    /*
-                    if (_target != null)
-                    {
-                        Destroy(_target.gameObject);
-                    }
-                    */
+                    float distance_to_player = Vector3.Distance(transform.position, player.transform.position);
+                    Vector3 player_location = player.transform.position;
+                    agent.SetDestination(player_location);
 
-                    // play laser beam
+                        if (distance_to_player <= 1)
+                        {
+                            _currentState = State.Attack;
+                        }
+                    break;
 
-                    _currentState = DroneState.Patrol;
+                }
+            case State.Attack:
+                {
+                    //Debug.Log("attacking");
+                    player.active = false;
                     break;
                 }
         }
     }
 
-    private bool IsPathBlocked()
+
+    public enum State
     {
-        Ray ray = new Ray(transform.position, _direction);
-        var hitSomething = Physics.RaycastAll(ray, _rayDistance, _layerMask);
-        return hitSomething.Any();
+        Patrol,
+        Patrol_Random,
+        Follow,
+        Attack
     }
-
-    private void GetDestination()
-    {
-        Vector3 testPosition = (transform.position + (transform.forward * 4f)) +
-                               new Vector3(UnityEngine.Random.Range(-4.5f, 4.5f), 0f,
-                                   UnityEngine.Random.Range(-4.5f, 4.5f));
-
-        _destination = new Vector3(testPosition.x, 1f, testPosition.z);
-
-        _direction = Vector3.Normalize(_destination - transform.position);
-        _direction = new Vector3(_direction.x, 0f, _direction.z);
-        _desiredRotation = Quaternion.LookRotation(_direction);
-    }
-
-    private bool NeedsDestination()
-    {
-        if (_destination == Vector3.zero)
-            return true;
-
-        var distance = Vector3.Distance(transform.position, _destination);
-        if (distance <= _stoppingDistance)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-
-
-    Quaternion startingAngle = Quaternion.AngleAxis(-60, Vector3.up);
-    Quaternion stepAngle = Quaternion.AngleAxis(5, Vector3.up);
-
-    private Transform CheckForAggro()
-    {
-        float aggroRadius = 5f;
-
-        RaycastHit hit;
-        var angle = transform.rotation * startingAngle;
-        var direction = angle * Vector3.forward;
-        var pos = transform.position;
-        for (var i = 0; i < 24; i++)
-        {
-            if (Physics.Raycast(pos, direction, out hit, aggroRadius))
-            {
-                if (1 == 2)
-                {
-                    Debug.Log("I have broke maths");
-                }
-                //var drone = hit.collider.GetComponent<Drone>();
-                /*
-                if (drone != null && drone.Team != gameObject.GetComponent<Drone>().Team)
-                {
-                    Debug.DrawRay(pos, direction * hit.distance, Color.red);
-                    return drone.transform;
-                }
-                */
-                else
-                {
-                    Debug.DrawRay(pos, direction * hit.distance, Color.yellow);
-                }
-            }
-            else
-            {
-                Debug.DrawRay(pos, direction * aggroRadius, Color.white);
-            }
-            direction = stepAngle * direction;
-        }
-
-        return null;
-    }
-}
-
-public enum Team
-{
-    Red,
-    Blue
-}
-
-public enum DroneState
-{
-    Patrol,
-    Patrol_Random,
-    Follow,
-    Attack
 }
