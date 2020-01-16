261e59725f50d34d01845961a1c511bea6e0d52f
Cole Gilbert <cg233192@falmouth.ac.uk>
2020-01-16 12:13:03 +0000

    Started working on sword attacks and blocking  - Camera Shaking complete  - Attacks can be held for a heavy attack

---
+++ Bone Rush/Assets/Bone Rush/Scripts/Cole's Pile of Wack Functions/SwordThings.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordThings : MonoBehaviour
+{
+
+	float attackHoldTime;
+	bool countAttackTime;
+	bool startedCounting;
+
+	CameraShake cameraShake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		cameraShake = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShake>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			countAttackTime = true;
+			startedCounting = true;
+		}
+		else if (Input.GetKey(KeyCode.Mouse0))
+		{
+			if (!cameraShake.shaking)
+			{
+				StartCoroutine(cameraShake.Shake(.2f));
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.Mouse0))
+		{
+			countAttackTime = false;
+		}
+
+
+		if (countAttackTime)
+		{
+			attackHoldTime += Time.deltaTime;
+		}
+		else if (startedCounting == true)
+		{
+			startedCounting = false;
+			Attack(attackHoldTime);
+			attackHoldTime = 0;
+		}
+    }
+
+	void Attack(float time)
+	{
+		
+	}
+}
