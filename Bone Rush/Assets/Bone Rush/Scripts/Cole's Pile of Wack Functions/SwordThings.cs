571fbded9a77045a946062b7edfbc709b743fd9b
ColeGGilbert <cg233192@falmouth.ac.uk>
2020-01-19 17:30:58 +0000

    Added more sword functions  - Swinging left and right is working  - Added delays to attacking to stop the sword animations from breaking  - Sword will reset it's position if held on the off-hand for too long

--- Bone Rush/Assets/Bone Rush/Scripts/Cole's Pile of Wack Functions/SwordThings.cs
+++ Bone Rush/Assets/Bone Rush/Scripts/Cole's Pile of Wack Functions/SwordThings.cs
@@ -8,6 +8,10 @@ public class SwordThings : MonoBehaviour
 	float attackHoldTime;
 	bool countAttackTime;
 	bool startedCounting;
+	float timePassedSinceAttacking;
+	float attackDelay;
+
+	Animator swordAnimation;
 
 	CameraShake cameraShake;
 
@@ -15,28 +19,38 @@ public class SwordThings : MonoBehaviour
     void Start()
     {
 		cameraShake = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShake>();
+		swordAnimation = GameObject.FindGameObjectWithTag("PlayerWeapon").GetComponent<Animator>();
+		swordAnimation.SetBool("Left?", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Mouse0))
+		if(attackDelay <= 0)
 		{
-			countAttackTime = true;
-			startedCounting = true;
-		}
-		else if (Input.GetKey(KeyCode.Mouse0))
-		{
-			if (!cameraShake.shaking)
+			if (Input.GetKeyDown(KeyCode.Mouse0))
 			{
-				StartCoroutine(cameraShake.Shake(.2f));
+				countAttackTime = true;
+				startedCounting = true;
+				timePassedSinceAttacking = 0f;
+			}
+			else if (Input.GetKey(KeyCode.Mouse0))
+			{
+				if (!cameraShake.shaking)
+				{
+					StartCoroutine(cameraShake.Shake(.2f));
+				}
+				timePassedSinceAttacking = 0f;
+			}
+			if (Input.GetKeyUp(KeyCode.Mouse0))
+			{
+				countAttackTime = false;
 			}
 		}
-		if (Input.GetKeyUp(KeyCode.Mouse0))
+		else
 		{
-			countAttackTime = false;
+			attackDelay -= Time.deltaTime;
 		}
-
 
 		if (countAttackTime)
 		{
@@ -48,10 +62,41 @@ public class SwordThings : MonoBehaviour
 			Attack(attackHoldTime);
 			attackHoldTime = 0;
 		}
-    }
+		timePassedSinceAttacking += Time.deltaTime;
+
+		if(timePassedSinceAttacking >= 1.5f && !swordAnimation.GetBool("Left?"))
+		{
+			swordAnimation.SetBool("Swing", false);
+			swordAnimation.SetBool("ResetPos", true);
+			swordAnimation.SetBool("Left?", !swordAnimation.GetBool("Left?"));
+		}
+		else if(swordAnimation.GetBool("ResetPos") && !swordAnimation.GetCurrentAnimatorStateInfo(0).IsName("SwordSwing"))
+		{
+			swordAnimation.SetBool("ResetPos", false);
+		}
+
+		if (swordAnimation.GetBool("Left?") && swordAnimation.GetCurrentAnimatorStateInfo(0).IsName("SwordSwing 0"))
+		{
+			swordAnimation.SetBool("Swing", false);
+		}
+		else if (!swordAnimation.GetBool("Left?") && swordAnimation.GetCurrentAnimatorStateInfo(0).IsName("SwordSwing"))
+		{
+			swordAnimation.SetBool("Swing", false);
+		}
+	}
 
 	void Attack(float time)
 	{
-		
+		attackDelay = .3f;
+		swordAnimation.SetBool("Left?", !swordAnimation.GetBool("Left?"));
+		swordAnimation.SetBool("Swing", true);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.transform.root != transform.root) // Checks that it is not colliding with the player
+		{
+			// Do stuff
+		}
 	}
 }
