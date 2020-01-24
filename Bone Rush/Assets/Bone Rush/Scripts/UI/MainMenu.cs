bcf76b5951b9872a7d6ff56ce86f678043a9d386
lr228874 <lr228874@falmouth.ac.uk>
2020-01-24 18:46:38 +0000

    fixed bug that made the cursor not appear on the game over scene

--- Bone Rush/Assets/Bone Rush/Scripts/UI/MainMenu.cs
+++ Bone Rush/Assets/Bone Rush/Scripts/UI/MainMenu.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("FirstLevel");
     }
     // Loads the next scene in the build order.
     // Ensure that the game scene is immediately after the MainMenu scene in the build order.
