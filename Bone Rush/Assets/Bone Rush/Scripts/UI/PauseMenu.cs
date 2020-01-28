8744f3bbdbab4fa8d8adeccdbdd66bb81fb482eb
Jamie C <JC228467@falmouth.ac.uk>
2020-01-28 19:15:38 +0000

    UI: -Made a pause menu in FirstLevel scene with Play, Options, Help, Extras and Main Menu buttons. -Made prefabs for UI elements. -Created PauseMenu.cs script.

---
+++ Bone Rush/Assets/Bone Rush/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // This allows it to be accessed by any other class.
+    #region Singleton
+    public static PauseMenu Instance;
+    private void Awake()
+    {
+        if (Instance != null) Destroy(this);
+        else Instance = this;
+    }
+    #endregion
+
+    // SerializeField allows you to see a private variable in the inspector.
+    [SerializeField]
+    private GameObject pauseMenu;
+    [SerializeField]
+    private GameObject healthBar;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) PauseGame();
+    }
+
+    public void PauseGame()
+    {
+        // Inverts function.
+        isPaused = !isPaused;
+
+        // If the game is paused, the timeScale is set to 0 so it actually pauses the game.
+        if (isPaused) Time.timeScale = 0f;
+        else Time.timeScale = 1f;
+
+        // Activates pauseMenu GO.
+        pauseMenu.SetActive(isPaused);
+        healthBar.SetActive(!isPaused);
+        Cursor.visible = isPaused;
+    }
+}
