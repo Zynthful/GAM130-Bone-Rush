fb15b74298136280f05c61dac9175958d776359c
Cole Gilbert <cg233192@falmouth.ac.uk>
2020-01-31 16:06:42 +0000

    Applying a fix to Boss  - Fixed sledgehammer animations  - Attack animation now plays in attack state

--- Bone Rush/Assets/Bone Rush/Scripts/animation.cs
+++ Bone Rush/Assets/Bone Rush/Scripts/animation.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class animation : MonoBehaviour
 {
-    public Animator swing;
-    private Animation anim;
+    private Animator anim;
+
+    private float testDelay;
+    private float switchIdle;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GameObject.Find("Handle").GetComponent<Animation>();
+        anim = GameObject.Find("Handle").GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.Play("BossSwing");
+        if (testDelay <= 0)
+        {
+            anim.SetBool("Attacking", true);
+            testDelay = 2f;
+            switchIdle = .5f;
+        }
+        else
+        {
+            testDelay -= Time.deltaTime;
+            switchIdle -= Time.deltaTime;
+        }
+
+        if(switchIdle <= 0)
+        {
+            anim.SetBool("Attacking", false);
+        }
     }
 }
